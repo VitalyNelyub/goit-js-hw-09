ef2c2d71975a9c8afb31233924d490a6721ec3d9
Vitaly <nelyub86@ukr.net>
2023-02-03 14:08:55 +0200

    finish HW #9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,62 +1,32 @@
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
-const btnSubmit = document.querySelector('button[type=submit]');
-
 const inputFirstDelay = document.querySelector('input[name=delay]');
 const inputStepDelay = document.querySelector('input[name=step]');
 const inputAmountCreatePromise = document.querySelector('input[name=amount]');
 
-// form.addEventListener('change', dataFotmInput);
 form.addEventListener('submit', submitForm);
 
-// let inputValues = [];
-
-// function dataFotmInput(e) {
-//   inputValues.push(e.target.value);
-//   console.log(e.target.value);
-// }
-
 function submitForm(e) {
-  // console.log(e);
-  // console.log(inputFirstDelay.value);
   let delay = inputFirstDelay.value;
-  console.log(inputStepDelay.value);
-  // console.log(inputAmountCreatePromise.value);
+
   e.preventDefault();
-  // let delay = inputFirstDelay.value;
+
   for (let i = 1; i <= inputAmountCreatePromise.value; i++) {
     let position = i;
-    console.log(position);
 
-    console.log(delay);
     createPromise(position, delay);
-    delay = Number(inputFirstDelay.value) + Number(inputStepDelay.value);
+    delay = Number(delay) + Number(inputStepDelay.value);
   }
 }
-
-// console.log(inputValues);
 
 function createPromise(position, delay) {
   setTimeout(() => {
     const shouldResolve = Math.random() > 0.3;
     if (shouldResolve) {
       Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
     } else {
       Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
     }
   }, delay);
 }
-
-// function createPromise(position, delay) {
-//   setTimeout(() => {
-//     const shouldResolve = Math.random() > 0.3;
-//     if (shouldResolve) {
-//       resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//     } else {
-//       reject(`❌ Rejected promise ${position} in ${delay}ms`);
-//     }
-//   }, delay);
-// }
